0cca974e7223799d449093840b18020b807fd34e
Chris Bird <chris@christopherbird.co.uk>
2014-11-26 16:26:39 +0000

    Fixed github paths

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 vagrant up 
-vagrant ssh -c “git clone http://github.com/chrisabird/ansible_desktop && ./dev_env/configure.sh”
+vagrant ssh -c “git clone https://github.com/chrisabird/ansible_desktop.git && ./ansible_desktop/configure.sh”
 vagrant reload
