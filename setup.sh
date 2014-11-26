29d0266bb911ab1aef847882c3fd6bbf14548ba5
Chris Bird <chris@christopherbird.co.uk>
2014-11-26 17:38:43 +0000

    Fixed config file and setup syntax error

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 vagrant ssh -- << ENDSSH
-git clone https://github.com/chrisabird/ansible_desktop.git &
-cd ansible_desktop &
+git clone https://github.com/chrisabird/ansible_desktop.git &&
+cd ansible_desktop &&
 ./configure.sh
 ENDSSH
