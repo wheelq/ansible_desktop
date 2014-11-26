b51a13ed4dc931700958b8f00e7df9cbb49bc729
Chris Bird <chris@christopherbird.co.uk>
2014-11-26 17:19:38 +0000

    added missing python headers

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get -y install git python python-yaml python-jinja2 aptitude python-pip
+sudo apt-get -y install git build-essential python python-dev python-yaml python-jinja2 aptitude python-pip
 sudo pip install ansible
