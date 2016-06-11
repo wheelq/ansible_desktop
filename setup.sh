8db7f70853b6de098e70ffa94aea09242c607405
wheelq <wheelq@gmail.com>
2016-06-11 21:06:30 +0100

    test
    test

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 vagrant ssh -- << ENDSSH
-git clone https://github.com/chrisabird/ansible_desktop.git &&
+git clone https://github.com/wheelq/ansible_desktop.git &&
 cd ansible_desktop &&
 ./configure.sh
 ENDSSH
